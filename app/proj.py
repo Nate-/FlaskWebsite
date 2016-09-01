8439a6bd858d3d096ba5d7c7754df7bb5dae8fad
Nate <nnq@hawaii.edu>
2016-09-01 22:21:38 +0000

    Project class to be used with Project template

---
+++ app/proj.py
@@ -0,0 +1,6 @@
+class Project(object):
+    def __init__(self, projName, projLink, projDesc, projTags):
+        self.name = projName
+        self.link = projLink
+        self.desc = projDesc
+        self.tags = projTags
