159d5cc3091288caa8f09f789680ee436243b2ce
Nate <nnq@hawaii.edu>
2016-09-01 22:27:23 +0000

    Added docstring for Project class usage

--- app/proj.py
+++ app/proj.py
@@ -1,5 +1,13 @@
 class Project(object):
     def __init__(self, projName, projLink, projDesc, projTags):
+        """
+        Stores input for use with project template
+
+        :type projName: str
+        :type projLink: str
+        :type projDesc: str
+        :type projTags: List[str] (techs used to build project) 
+        """
         self.name = projName
         self.link = projLink
         self.desc = projDesc
